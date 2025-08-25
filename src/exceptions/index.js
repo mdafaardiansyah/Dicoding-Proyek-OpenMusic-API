7e84232293465a2ec65cd7c81e334fc2a925aae2
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-08-25 17:54:30 +0700

    feat(exceptions): add AuthenticationError and AuthorizationError classes
    Add new error classes for handling 401 and 403 HTTP status codes respectively. These classes extend ClientError and are exported in the exceptions index file.

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -5,9 +5,13 @@
 const ClientError = require('./ClientError');
 const NotFoundError = require('./NotFoundError');
 const InvariantError = require('./InvariantError');
+const AuthenticationError = require('./AuthenticationError');
+const AuthorizationError = require('./AuthorizationError');
 
 module.exports = {
   ClientError,
   NotFoundError,
   InvariantError,
+  AuthenticationError,
+  AuthorizationError,
 };
