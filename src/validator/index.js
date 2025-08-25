1eb000eaf98857265c18d30da92e64eec28d41f9
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-08-25 17:54:05 +0700

    feat(validator): add new validator modules for users, authentications, playlists and collaborations
    Add schemas for authentication payloads and export new validator modules to support additional features in the application.

--- src/validator/index.js
+++ src/validator/index.js
@@ -5,8 +5,16 @@
 
 const AlbumsValidator = require('./albums');
 const SongsValidator = require('./songs');
+const UsersValidator = require('./users');
+const AuthenticationsValidator = require('./authentications');
+const PlaylistsValidator = require('./playlists');
+const CollaborationsValidator = require('./collaborations');
 
 module.exports = {
   AlbumsValidator,
   SongsValidator,
+  UsersValidator,
+  AuthenticationsValidator,
+  PlaylistsValidator,
+  CollaborationsValidator,
 };
