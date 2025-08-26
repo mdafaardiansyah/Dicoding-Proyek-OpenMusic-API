d9333693f6fa9e9b2e1d27752025bc0ee27de20c
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-08-26 13:10:28 +0700

    fix(server): change default host to '0.0.0.0' for production consistency
    Simplify host configuration by removing environment check and always using '0.0.0.0' to ensure consistent behavior across environments

--- src/server.js
+++ src/server.js
@@ -54,7 +54,7 @@ const init = async () => {
   // Konfigurasi server Hapi.js
   const server = Hapi.server({
     port: process.env.PORT || 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
