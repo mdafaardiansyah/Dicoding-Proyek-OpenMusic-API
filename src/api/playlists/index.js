a9eca310b9ae072b614946b797303531cd1671bc
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-08-28 17:23:43 +0700

    refactor(playlists): add cacheService parameter to PlaylistsHandler
    The cacheService parameter was added to enable caching functionality in the playlists module, improving performance by reducing redundant database queries.

--- src/api/playlists/index.js
+++ src/api/playlists/index.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 module.exports = {
   name: 'playlists',
   version: '1.0.0',
-  register: async (server, { service, validator, songsService }) => {
-    const playlistsHandler = new PlaylistsHandler(service, validator);
+  register: async (server, { service, validator, cacheService }) => {
+    const playlistsHandler = new PlaylistsHandler(service, validator, cacheService);
     server.route(routes(playlistsHandler));
   },
 };
