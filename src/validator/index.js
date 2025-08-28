2b36c1e9175601f1eae8ff722831acb1b2eb0e84
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-08-28 17:00:25 +0700

    feat(validator): add exports and uploads validators

--- src/validator/index.js
+++ src/validator/index.js
@@ -9,6 +9,8 @@ const UsersValidator = require('./users');
 const AuthenticationsValidator = require('./authentications');
 const PlaylistsValidator = require('./playlists');
 const CollaborationsValidator = require('./collaborations');
+const ExportsValidator = require('./exports');
+const UploadsValidator = require('./uploads');
 
 module.exports = {
   AlbumsValidator,
@@ -17,4 +19,6 @@ module.exports = {
   AuthenticationsValidator,
   PlaylistsValidator,
   CollaborationsValidator,
+  ExportsValidator,
+  UploadsValidator,
 };
